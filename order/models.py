a929d2fc28bab33b4f1e29ecd9954a84680820b9
Aigerim <maviboncuaika@gmail.com>
2022-05-05 11:56:24 +0600

    models

---
+++ order/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from account.models import User
+from service.models import Category
+
+class Order(models.Model):
+    user = models.ForeignKey(User)
+    address = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Entries(models.Model):
+    user = models.ForeignKey(User)
+    category = models.ForeignKey(Category)
+
+
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        Category, related_name='products', on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+    image = models.ImageField(upload_to='media', blank=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveSmallIntegerField(default=0)
+    available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+
+
+
+
