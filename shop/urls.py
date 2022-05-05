c502874d282c97d090329c520f86b4880a4ca6c7
miracle <umuraliev.r@gmail.com>
2022-05-05 23:55:36 +0600

    fixed filter and some html

--- shop/urls.py
+++ shop/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.get_product_list, name='products_list'),
     path('search_product/', views.search_product, name='search'),
+    path('<str:category_slug>/', views.get_product_list, name='product_list_by_category'),
 ]
