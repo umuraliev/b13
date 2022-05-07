483d430d2b4fd48b725ea4887d9e9fb801c499af
Aigerim <maviboncuaika@gmail.com>
2022-05-07 23:09:02 +0600

    service

--- service/urls.py
+++ service/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.get_services_list, name='services_list'),
-
+    path('<str:category_slug>/', views.get_barbers_list, name='barber_list_by_category'),
 ]
